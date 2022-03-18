9c39e2b6252cb605bb3759528bcfb5eb0a40b776
yadubabu <100501154+yadubabu@users.noreply.github.com>
2022-03-18 21:10:15 +0530

    initial commit

---
+++ express_crash_course/members.js
@@ -0,0 +1,21 @@
+const members=[
+    {
+        id:1,
+        name:'John Deo',
+        email:'johndeo@gmail.com',
+        status:'active'
+    },
+    {
+        id:2,
+        name:'Bob Williums',
+        email:'bobwilliums@gmail.com',
+        status:'inactive'
+    },
+    {
+        id:3,
+        name:'Shanoon Jackson',
+        email:'shanoonjackson@gmail.com',
+        status:'active'
+    }
+];
+module.exports=members;
